3615cbddfb63e194700e8f4b6287a17a4e7def46
Tayna Rodrigues <taynajesus@gmail.com>
2020-02-19 16:21:25 -0400

    Validadores e Matchers

--- ruby/unitarios/spec/avengers_spec.rb
+++ ruby/unitarios/spec/avengers_spec.rb
@@ -19,7 +19,50 @@ describe AvengersHeadQuarter do
 		hq = AvengersHeadQuarter.new #O hq é a instancia de uma classe
 
 		hq.put('Spiderman')
-		expect(hq.list).to eql ['Spiderman']
+		expect(hq.list).to include 'Spiderman'
+			# expect(hq.list).to eql ['Spiderman'] #include -> se contém na lista
 	end
 
+	it 'deve aicionar uma lista de vingadores' do
+		hq = AvengersHeadQuarter.new
+		hq.put('Thor')
+		hq.put('Hulk')
+		hq.put('Spiderman')
+
+		# expect(hq.list).to include 'Hulk'
+		res = hq.list.size > 0
+
+		expect(hq.list.size).to be > 0 #verificando se existe uma lista; o método size só funciona pra listas
+		expect(res).to be true
+	end
+
+	it 'thor deve ser o primeiro da lista' do
+		hq = AvengersHeadQuarter.new
+
+		hq.put('Thor')
+		hq.put('Hulk')
+		hq.put('Spiderman')
+
+		expect(hq.list).to start_with('Thor')
+
+	end
+
+	it 'Ironman deve ser o último da lista' do
+			hq = AvengersHeadQuarter.new
+
+			hq.put('Thor')
+			hq.put('Hulk')
+			hq.put('Spiderman')
+			hq.put('Ironman')
+
+			expect(hq.list).to end_with('Ironman')
+	end
+
+	it 'deve conter o sobrenome' do
+
+		avenger = 'Peter Parker'
+
+		expect(avenger).to match(/Parker/)
+		expect(avenger).not_to match(/Papito/)
+	end
 end
