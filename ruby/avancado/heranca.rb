7b716e01a77e2d774efa2529d8668c26a2e46839
Tayna Rodrigues <taynajesus@gmail.com>
2020-02-19 11:52:30 -0400

    refactoring código para Herança

--- ruby/avancado/heranca.rb
+++ ruby/avancado/heranca.rb
@@ -1,30 +1,4 @@
-class Carro
-	#Atributo
- attr_accessor :nome, :marca, :modelo
-
- #Construtor
-	def initialize(nome, marca, modelo)
-			self.nome = nome
-			self.marca = marca
-			self.modelo = modelo
-	end
-
-	#Métodos
-	def ligar
-		puts "#{nome} esta pronto para iniciar o trajeto!"
-	end
-
-	def buzinar
-		puts 'Beep! Beep!'
-	end
-
-	def dirigir
-		puts "#{nome} iniciando o trajeto"
-	end
-end
-
-
- class Moto
+class Veiculo
 		attr_accessor :nome, :marca, :modelo
 
 		def initialize(nome, marca, modelo)
@@ -34,16 +8,28 @@ end
 		end
 
 		def ligar
-			puts "#{nome} está pronto para iniciar o trajeto"
+			puts "#{nome} está prontp para iniciar o projeto"
 		end
-
+		
 		def buzinar
 			puts 'Beep! Beep!'
 		end
+end
 
+class Carro < Veiculo
+
+		def dirigir
+			puts "#{nome} iniciando o trajeto"
+		end
+	end
+
+
+ class Moto < Veiculo #o seinal de menor representa que está Herdando da classe veículo
+		
 		def pilotar
 			puts "#{nome} iniciando o trajeto na moto."
- end
+		 end
+	end
 
 
 civic = Carro.new('Civic', 'Honda', 'SI')
@@ -58,7 +44,7 @@ lancer.buzinar
 lancer.dirigir
 
 #Criando mais um objeto
-fazer = Moto.new('Fazer', 'Yamaha', '250')
+fazer = Moto.new('Fazer', 'Yamaha', '250x')
 fazer.ligar
 fazer.buzinar
-fazer.dirigir
+fazer.pilotar
