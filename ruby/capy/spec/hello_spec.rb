60e6d4f5a6826c054302e11fea55d28b2e0f62e0
Tayna Rodrigues <taynajesus@gmail.com>
2020-02-21 11:26:10 -0400

    Dominando Capybara: Meu segundo script de automação

--- ruby/capy/spec/hello_spec.rb
+++ ruby/capy/spec/hello_spec.rb
@@ -1,9 +1,9 @@
 describe 'Meu primeiro script' do
 
 	it 'visitar a página' do
-		
-		visit 'https://training-wheels-protocol.herokuapp.com/'
-		expect(page.title).to eql 'Training Wheels Protocol'
+			
+		visit 'http://192.168.151.84/pemaza/login.php?msg=deslogado'
+		expect(page.title).to eql 'PemazaWeb®'
 
 	end
 end
