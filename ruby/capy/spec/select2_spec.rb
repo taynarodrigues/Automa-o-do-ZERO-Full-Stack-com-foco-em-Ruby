ccc40f6aaa95ab54e0b1f08b43e8ff98eea1269a
Tayna Rodrigues <taynajesus@gmail.com>
2020-03-23 15:04:37 -0400

    Selecionando Opção com Select2: multiple

--- ruby/capy/spec/select2_spec.rb
+++ ruby/capy/spec/select2_spec.rb
@@ -22,11 +22,44 @@ describe 'Select2', :select2 do
 		end
 	end
 
-	describe('multiple') do
+	describe('multiple', :mult) do
 
 		before(:each) do
 
 			visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/multi.html'
 		end
+
+		# it 'seleciona atores' do
+		# 	find('.select2-selection--multiple').click
+		# 	sleep 1
+
+		# 	find('.select2-search__field').set 'Jim Carrey'
+		# 	find('.select2-results__option').click
+		# 	sleep 5
+		# end
+
+
+
+				def selecione(ator) 
+					find('.select2-selection--multiple').click #Nesse caso os elementos estão sendo encapsulados dentro do método!
+					find('.select2-search__field').set ator
+					find('.select2-results__option').click
+				end
+
+				it 'seleciona atores' do
+
+					# selecione('Jim Carrey')
+					# selecione('Owen Wilson')
+					# sleep 5
+
+					#Otimizando com array
+
+					atores = ['Jim Carrey', 'Owen Wilson', 'kevin James']
+
+					atores.each do |a|
+						selecione(a)
+					end
+					sleep 5
+			end
 	end
 end
