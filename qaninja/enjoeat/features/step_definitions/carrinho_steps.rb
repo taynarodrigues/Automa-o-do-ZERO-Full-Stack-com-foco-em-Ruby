3ed47d65199532c938d31a42daae807a4057e905
Tayna Rodrigues <taynajesus@gmail.com>
2020-04-12 18:49:42 -0400

    Instanciando uma classe de pageObject no método Before, fazendo com que tenha a camada de steps definition com redução de código e de fácil implementação

--- qaninja/enjoeat/features/step_definitions/carrinho_steps.rb
+++ qaninja/enjoeat/features/step_definitions/carrinho_steps.rb
@@ -13,14 +13,14 @@ Quando("eu adiciono {int} unidade\\(s)") do |quantidade|
     end
 
 Então("deve ser adicionado {int} unidade\\(s) deste item") do |quantidade|
-    cart_page = CartPage.new
-    expect(cart_page.box).to have_text "(#{quantidade}x) #{@produto_nome}" 
+    @cart_page = CartPage.new
+    expect(@cart_page.box).to have_text "(#{quantidade}x) #{@produto_nome}" 
     puts "(#{quantidade}x) #{@produto_nome}"
 end
 
 Então("o valor total deve ser de {string}") do |valor_total|
-    cart_page = CartPage.new
-    total = cart_page.box.find('tr', text: 'Total').find('td') 
+    @cart_page = CartPage.new
+    total = @cart_page.box.find('tr', text: 'Total').find('td') 
     expect(total.text).to eql valor_total
 end
 
@@ -39,9 +39,8 @@ Quando("eu adiciono todos os itens") do
 end
 
 Então("vejo todos os itens no carrinho") do
-    cart_page = CartPage.new
     @product_list.each do |p|
-        expect(cart_page.box).to have_text "(#{p["quantidade"]}x) #{p["nome"]}"
+        expect(@cart_page.box).to have_text "(#{p["quantidade"]}x) #{p["nome"]}"
     end
 end
 
@@ -56,16 +55,14 @@ Dado("que eu tenho os seguintes itens no carrinho") do |table|
 end
 
 Dado("eu removo somente o {int}") do |item|
-    cart_page = CartPage.new
-    cart_page.box.all("table tbody tr")[item].find(".danger").click
+    @cart_page.box.all("table tbody tr")[item].find(".danger").click
 end
 
 #Remover todos os itens
 
 Quando("eu removo todos os itens") do
-    cart_page = CartPage.new
     @product_list.each_with_index do |value, indx| 
-        cart_page.box.all("table tbody tr")[indx].find(".danger").click
+        @cart_page.box.all("table tbody tr")[indx].find(".danger").click
     end
 end
 
@@ -74,6 +71,5 @@ Quando("eu limpo o meu carrinho") do
 end
 
 Então("vejo a seguinte mensagem no carrinho {string}") do |mensagem|
-    cart_page = CartPage.new
-    expect(cart_page.box).to have_text mensagem
+    expect(@cart_page.box).to have_text mensagem
 end
--- qaninja/enjoeat/features/support/hooks.rb
+++ qaninja/enjoeat/features/support/hooks.rb
@@ -1,7 +1,8 @@
 Before do 
     page.current_window.resize_to(1440, 900) #código para redimensionar à página
-end
 
+    @cart_page = CartPage.new  #instancia a CartPage.new
+end
 
 Before("@bread_bakery") do #before para acessar o cardápio do restaurante através da tag @bread_bakery
     visit "/restaurants/bread-bakery/menu"
