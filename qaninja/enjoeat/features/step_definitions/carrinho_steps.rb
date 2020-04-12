44f4f9a2e4b9427729ad35fddd5d76e08dcb0314
Tayna Rodrigues <taynajesus@gmail.com>
2020-04-12 11:24:03 -0400

    Refatorando o código onde garante-se a assertividade do script de automação

--- qaninja/enjoeat/features/step_definitions/carrinho_steps.rb
+++ qaninja/enjoeat/features/step_definitions/carrinho_steps.rb
@@ -12,15 +12,12 @@ end
 
 Então("deve ser adicionado {int} unidade deste item") do |quantidade|
     cart = find('#cart')
-    expect(cart).to have_text "(#{quantidade}x) #{@produto_nome}" # (1x) Cup Cake #Usando interpolação de string para monta uma verificação
+    expect(cart).to have_text "(#{quantidade}x) #{@produto_nome}" 
     puts "(#{quantidade}x) #{@produto_nome}"
 end
 
 Então("o valor total deve ser de {string}") do |valor_total|
     cart = find('#cart')
-    total = cart.find('tr', text: 'Total').find('td')  # $('#cart tr:contains("Total:") td'); -> replicando código de DQuery
-    expect(cart).to have_text valor_total
-    puts total.text
-    puts valor_total
-    sleep 5
+    total = cart.find('tr', text: 'Total').find('td') 
+    expect(total.text).to eql valor_total
 end
